07451fe0bce6ac349dbf7ff412e79c99009cae27
Sergey Malyuga <sergey@malyuga.ru>
2026-09-09 15:57:05 +0300

    feat(home-page): добавить отображение офферов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {AppState} from './core/models/app.state';
+import {loadOffers} from './store/offer/actions/offer.actions';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'angular-template-project';
+export class AppComponent implements OnInit {
+  private store = inject(Store<AppState>);
+
+  public ngOnInit(): void {
+    this.store.dispatch(loadOffers());
+  }
 }
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,14 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, inject} from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
+import {AppState} from '../../core/models/app.state';
+import {Store} from '@ngrx/store';
+import {selectCurrentCity, selectOffersByCity} from '../../store/app/selectors/app.selectors';
+import {OfferCardComponent} from '../../shared/components/offer-card/offer-card.component';
+import {CITY_LOCATIONS} from '../../core/constants/const';
+import {NgClass} from '@angular/common';
+import {City} from '../../core/models/city';
 
 @Component({
   selector: 'app-main',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    OfferCardComponent,
+    NgClass
   ],
   templateUrl: './main.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainComponent {
+  private store = inject(Store<AppState>);
+  protected readonly CITY_LOCATIONS = CITY_LOCATIONS;
 
+  public offers = this.store.selectSignal(selectOffersByCity);
+  public offersTotal = computed(() => this.offers().length);
+  public currentCity = this.store.selectSignal(selectCurrentCity);
+
+  public isActiveCity(city: City): boolean {
+    return city.name === this.currentCity().name
+  }
 }
