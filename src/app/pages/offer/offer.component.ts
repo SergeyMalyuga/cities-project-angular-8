9d471c7518237ba2315375d53290e2eab65c4d0b
Sergey Malyuga <sergey@malyuga.ru>
2026-09-09 12:22:35 +0300

    feat(offer): создать компонент страницы оффера

---
+++ src/app/pages/offer/offer.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {HeaderComponent} from '../../shared/components/header/header.component';
+
+@Component({
+  selector: 'app-offer',
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './offer.component.html',
+  styleUrl: './offer.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class OfferComponent {
+
+}
