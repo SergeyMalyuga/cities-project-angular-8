4aea33d8b8ed695f7847cd4d325ce84462d3a7e7
Sergey Malyuga <sergey@malyuga.ru>
2026-09-09 13:14:08 +0300

    feat(routes): добавить основную маршрутизацию в проект

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AppRoute} from './core/constants/const';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: AppRoute.MAIN,
+    title: 'Main',
+    loadComponent: () => import('./pages/main/main.component').then((m) => m.MainComponent)
+  },
+  {
+    path: AppRoute.LOGIN,
+    title: 'Login',
+    loadComponent: () => import('./pages/login/login.component').then((m) => m.LoginComponent)
+  },
+  {
+    path: AppRoute.FAVORITES,
+    title: 'Favorites',
+    loadComponent: () => import('./pages/favorites/favorites.component').then((m) => m.FavoritesComponent)
+  },
+  {
+    path: `${AppRoute.OFFER}/:id`,
+    title: 'Offer',
+    loadComponent: () => import('./pages/offer/offer.component').then((m) => m.OfferComponent)
+  },
+  {
+    path: '**',
+    title: '404 Not Found',
+    loadComponent: () => import('./pages/404/404.component').then((m) => m.NotFoundComponent)
+  }
+];
