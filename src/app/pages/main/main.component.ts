36db4825aa1390e71553421b24688184d7b8d6f0
Sergey Malyuga <sergey@malyuga.ru>
2026-09-09 12:22:07 +0300

    feat(main): создать компонент домашней страницы

---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {HeaderComponent} from '../../shared/components/header/header.component';
+
+@Component({
+  selector: 'app-main',
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './main.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MainComponent {
+
+}
