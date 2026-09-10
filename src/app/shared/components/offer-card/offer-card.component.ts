54094b4f0f94f61deb5bd3cdacab45ffacbe258d
Sergey Malyuga <sergey@malyuga.ru>
2026-09-10 14:57:01 +0300

    feat(offer-card): эмитить событие при наведении и снятия курсора с элемента

--- src/app/shared/components/offer-card/offer-card.component.ts
+++ src/app/shared/components/offer-card/offer-card.component.ts
@@ -1,17 +1,31 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 import {OfferPreview} from '../../../core/models/offers';
 import {getWidthByRating} from '../../../core/utils/width-by-rating';
 import {TitleCasePipe} from '@angular/common';
+import {HoverTrackerDirective} from '../../directives/hover-tracker.directive';
 
 @Component({
   selector: 'app-offer-card',
   imports: [
-    TitleCasePipe
+    TitleCasePipe,
+    HoverTrackerDirective
   ],
   templateUrl: './offer-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OfferCardComponent {
-@Input({required: true}) offer!: OfferPreview;
+  @Input({required: true}) offer!: OfferPreview;
+  @Input() isHoverEnable = false;
+
+  @Output() hovered = new EventEmitter<OfferPreview | null>();
+
   protected readonly getWidthByRating = getWidthByRating;
+
+  public onHovered(isHover: boolean) {
+    if (isHover) {
+      this.hovered.emit(this.offer)
+    } else {
+      this.hovered.emit(null);
+    }
+  }
 }
