79927cd5ee2c17fb4f0f98e318e34d8be3e78070
Sergey Malyuga <sergey@malyuga.ru>
2026-09-11 15:31:03 +0300

    feat(header): адаптировать отображение элементов для аутентифицированных пользователей

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,27 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {AppState} from '../../../core/models/app.state';
+import {selectAuthStatus, selectUserEmail} from '../../../store/user/selectors/user.selector';
+import {isAuth} from '../../../core/utils/auth-status';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../../core/constants/const';
+import {selectFavoriteOffersTotal} from '../../../store/favoritre-offer/selectors/favorite-offer.selectors';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent {
+  private store = inject(Store<AppState>);
 
+  protected readonly isAuth = isAuth;
+  protected readonly AppRoute = AppRoute;
+
+  public offersTotal = this.store.selectSignal(selectFavoriteOffersTotal);
+  public authStatus = this.store.selectSignal(selectAuthStatus);
+  public email = this.store.selectSignal(selectUserEmail);
 }
