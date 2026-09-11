32ad365cb47eff435a44627f56228cef9e62f9f7
Sergey Malyuga <sergey@malyuga.ru>
2026-09-11 15:19:53 +0300

    feat(user): добавить selector email пользователя

--- src/app/store/user/selectors/user.selector.ts
+++ src/app/store/user/selectors/user.selector.ts
@@ -7,3 +7,8 @@ export const selectAuthStatus = createSelector(
   selectUserState,
   state => state.authorizationStatus
 );
+
+export const selectUserEmail = createSelector(
+  selectUserState,
+  state => state.user?.email
+)
