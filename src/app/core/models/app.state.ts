b4f1f0ea574ccd3a8666440cbc29efaa1336a184
Sergey Malyuga <sergey@malyuga.ru>
2026-09-11 11:13:21 +0300

    feat(app): добавить состояние пользователя в глобальный стор

--- src/app/core/models/app.state.ts
+++ src/app/core/models/app.state.ts
@@ -1,12 +1,12 @@
-import { OfferState } from './offer-state';
-import { UserState } from './user.state';
-import { FavoriteOfferState } from './favorite-offer.state';
-import { City } from './city';
+import {OfferState} from './offer-state';
+import {UserState} from './user.state';
+import {FavoriteOfferState} from './favorite-offer.state';
+import {City} from './city';
 
 export interface AppState {
   offer: OfferState;
   currentCity: City;
-/*
   user: UserState;
-  favoriteOffer: FavoriteOfferState;*/
+  /*
+    favoriteOffer: FavoriteOfferState;*/
 }
--- src/app/store/app/app.reducer.ts
+++ src/app/store/app/app.reducer.ts
@@ -2,8 +2,10 @@ import {ActionReducerMap} from '@ngrx/store';
 import {AppState} from '../../core/models/app.state';
 import {offerReducer} from '../offer/offer.reducer';
 import {cityReducer} from '../city/city.reducer';
+import {userReducer} from '../user/user.reducer';
 
 export const appReducer: ActionReducerMap<AppState> = {
   offer: offerReducer,
   currentCity: cityReducer,
+  user: userReducer,
 }
