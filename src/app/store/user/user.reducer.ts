ab8fbe656ea56fbf46c9f954ce71ffdd1d15ff61
Sergey Malyuga <sergey@malyuga.ru>
2026-09-11 16:02:22 +0300

    feat(user): добавить обработку действий аутентификации

--- src/app/store/user/user.reducer.ts
+++ src/app/store/user/user.reducer.ts
@@ -1,7 +1,7 @@
 import {UserState} from '../../core/models/user.state';
 import {AuthorizationStatus, DEFAULT_USER} from '../../core/constants/const';
 import {createReducer, on} from '@ngrx/store';
-import {checkAuth, checkAuthFailure, checkAuthSuccess} from './actions/user.actions';
+import {checkAuth, checkAuthFailure, checkAuthSuccess, login, loginFailure, loginSuccess} from './actions/user.actions';
 
 const initialState: UserState = {
   user: DEFAULT_USER,
@@ -19,6 +19,16 @@ export const userReducer = createReducer(
     ...state, user, authorizationStatus: AuthorizationStatus.AUTH, isLoading: false, error: null
   })),
   on(checkAuthFailure, (state, {error}) => ({
+    ...state, authorizationStatus: AuthorizationStatus.UN_AUTH, isLoading: false, error
+  })),
+
+  on(login, state => ({
+    ...state, isLoading: true
+  })),
+  on(loginSuccess, (state, {user}) => ({
+    ...state, user, authorizationStatus: AuthorizationStatus.AUTH, isLoading: false, error: null
+  })),
+  on(loginFailure, (state, {error}) => ({
     ...state, authorizationStatus: AuthorizationStatus.UN_AUTH, isLoading: false, error
   }))
 );
