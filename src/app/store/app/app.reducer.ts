af3a5be6c0f83eab1a23e20676b7a725e9c8dcf6
Sergey Malyuga <sergey@malyuga.ru>
2026-09-11 15:21:02 +0300

    feat(app): добавить состояние избранных офферов в глобальный store

--- src/app/store/app/app.reducer.ts
+++ src/app/store/app/app.reducer.ts
@@ -3,9 +3,11 @@ import {AppState} from '../../core/models/app.state';
 import {offerReducer} from '../offer/offer.reducer';
 import {cityReducer} from '../city/city.reducer';
 import {userReducer} from '../user/user.reducer';
+import {favoriteOfferReducer} from '../favoritre-offer/favorite-offer.reducer';
 
 export const appReducer: ActionReducerMap<AppState> = {
   offer: offerReducer,
   currentCity: cityReducer,
   user: userReducer,
+  favoriteOffer: favoriteOfferReducer
 }
