85ed6d1b5bcba842a2eb70c71de059e9dc74c66d
Sergey Malyuga <sergey@malyuga.ru>
2026-09-11 11:11:52 +0300

    feat(config): подключить UserEffects, AuthInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,16 +3,23 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideStore} from '@ngrx/store';
-import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {provideEffects} from '@ngrx/effects';
 import {appReducer} from './store/app/app.reducer';
 import {OfferEffects} from './store/offer/effects/offer.effects';
+import {AuthInterceptor} from './core/interceptors/auth.interceptor';
+import {UserEffects} from './store/user/effects/user.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideStore(appReducer),
     provideHttpClient(withInterceptorsFromDi()),
-    provideEffects(OfferEffects),
+    provideEffects(OfferEffects, UserEffects),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    }
   ],
 };
