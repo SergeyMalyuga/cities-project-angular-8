e67eec646784a6151fc94268b588fccb667aa30d
Sergey Malyuga <sergey@malyuga.ru>
2026-09-11 15:29:59 +0300

    feat(favorite-offer): добавить получение общего количества избранных офферов

---
+++ src/app/store/favoritre-offer/selectors/favorite-offer.selectors.ts
@@ -0,0 +1,8 @@
+import {createFeatureSelector} from '@ngrx/store';
+import {AppState} from '../../../core/models/app.state';
+import {favoriteOfferAdapter} from '../favorite-offer.reducer';
+
+const selectFavoriteOfferState = createFeatureSelector<AppState['favoriteOffer']>('favoriteOffer');
+const favoriteOfferSelectors = favoriteOfferAdapter.getSelectors(selectFavoriteOfferState);
+
+export const selectFavoriteOffersTotal = favoriteOfferSelectors.selectAll;
