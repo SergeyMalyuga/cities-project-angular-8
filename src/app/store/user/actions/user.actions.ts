0c576cbf1faedf37c562017c832f32853224279b
Sergey Malyuga <sergey@malyuga.ru>
2026-09-11 16:01:50 +0300

    feat(user): добавить actions аутентификации

--- src/app/store/user/actions/user.actions.ts
+++ src/app/store/user/actions/user.actions.ts
@@ -1,9 +1,17 @@
 import {createAction, props} from '@ngrx/store';
 import {User} from '../../../core/models/user';
 import {HttpErrorResponse} from '@angular/common/http';
+import {Credentials} from '../../../core/models/credentials';
 
 export const checkAuth = createAction('[App Component] Check Auth');
 export const checkAuthSuccess = createAction('[App Component] Check Auth Success',
   props<{ user: User }>());
 export const checkAuthFailure = createAction('[App Component] Check Auth Failure',
   props<{ error: HttpErrorResponse | string }>());
+
+export const login = createAction('[Login Component] Login',
+  props<{ credentials: Credentials }>());
+export const loginSuccess = createAction('[Login Component] Login Success',
+  props<{ user: User }>());
+export const loginFailure = createAction('[Login Component] Login Failure',
+  props<{ error: HttpErrorResponse }>());
