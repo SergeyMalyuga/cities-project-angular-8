18b85314badbfb249208cdf6fde171d6300ce44c
Sergey Malyuga <sergey@malyuga.ru>
2026-09-14 11:58:50 +0300

    feat(user): добавить effect выхода из учётной записи

--- src/app/store/user/effects/user.effects.ts
+++ src/app/store/user/effects/user.effects.ts
@@ -2,7 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import {AuthService} from '../../../core/services/auth.service';
 import * as UserActions from '../actions/user.actions';
-import {catchError, map, of, switchMap} from 'rxjs';
+import {catchError, map, of, switchMap, tap} from 'rxjs';
 import {UserService} from '../../../core/services/user.service';
 import {HttpErrorResponse} from '@angular/common/http';
 import {loadFavoriteOffers} from '../../favoritre-offer/actions/favorite-offer.actions';
@@ -32,6 +32,13 @@ export class UserEffects {
 
   public login$ = createEffect(() => this.actions$.pipe(ofType(UserActions.login),
     switchMap(({credentials}) => this.userService.login(credentials)
-      .pipe(map(user => UserActions.loginSuccess({user})),
+      .pipe(tap(user => this.authService.setToken(user.token)), map(user => UserActions.loginSuccess({user})),
         catchError((error: HttpErrorResponse) => of(UserActions.loginFailure({error})))))));
+
+  public logout$ = createEffect(() =>
+    this.actions$.pipe(ofType(UserActions.logout),
+      switchMap(() =>
+        this.userService.logout().pipe(tap(() => this.authService.removeToken()),
+          map(() => UserActions.logoutSuccess()),
+          catchError((error: HttpErrorResponse) => of(UserActions.logoutFailure({error})))))));
 }
