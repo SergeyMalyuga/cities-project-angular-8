5b4e4c502ad88bd7363e7776365aa3cb491b7828
Sergey Malyuga <sergey@malyuga.ru>
2026-09-14 12:02:50 +0300

    fix(favorite-offer): исправить получение общего количества избранных офферов

--- src/app/store/favoritre-offer/selectors/favorite-offer.selectors.ts
+++ src/app/store/favoritre-offer/selectors/favorite-offer.selectors.ts
@@ -5,4 +5,4 @@ import {favoriteOfferAdapter} from '../favorite-offer.reducer';
 const selectFavoriteOfferState = createFeatureSelector<AppState['favoriteOffer']>('favoriteOffer');
 const favoriteOfferSelectors = favoriteOfferAdapter.getSelectors(selectFavoriteOfferState);
 
-export const selectFavoriteOffersTotal = favoriteOfferSelectors.selectAll;
+export const selectFavoriteOffersTotal = favoriteOfferSelectors.selectTotal;
