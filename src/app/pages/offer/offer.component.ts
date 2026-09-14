45353d3708d9983d44f8c36b871a90a8266f8512
Sergey Malyuga <sergey@malyuga.ru>
2026-09-14 16:03:21 +0300

    feat(offer): добавить загрузку оффера по id
    - Загрузка осуществляется при открытии страницы оффера

--- src/app/pages/offer/offer.component.ts
+++ src/app/pages/offer/offer.component.ts
@@ -1,14 +1,54 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, DestroyRef, inject, OnInit, signal} from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
+import {Offer} from '../../core/models/offers';
+import {catchError, combineLatest, EMPTY, filter, map, merge, Subject, switchMap} from 'rxjs';
+import {ActivatedRoute, Router} from '@angular/router';
+import {OfferDataService} from '../../core/services/offer-data.service';
+import {AppRoute} from '../../core/constants/const';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {getWidthByRating} from '../../core/utils/width-by-rating';
+import {TitleCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-offer',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    TitleCasePipe
   ],
   templateUrl: './offer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OfferComponent {
+export class OfferComponent implements OnInit {
+  private refreshOffer$ = new Subject<void>();
+  private activatedRoute = inject(ActivatedRoute);
+  private router = inject(Router);
+  private offerDataService = inject(OfferDataService);
+  private destroyRef = inject(DestroyRef);
 
+  public offer = signal<Offer | null>(null);
+  public offerId = computed(() => this.offer()?.id ?? null);
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.pipe(map(params => params.get('id')),
+      filter((id): id is string => id !== null),
+      switchMap(id => {
+
+        const offer$ = merge(
+          this.offerDataService.getOfferById(id),
+          this.refreshOffer$.pipe(switchMap(() => this.offerDataService.getOfferById(id).pipe(
+            catchError(() => {
+              return EMPTY;
+              this.router.navigate([AppRoute.MAIN])
+            })))));
+
+        return combineLatest({
+          offer: offer$
+        })
+      }), takeUntilDestroyed(this.destroyRef))
+      .subscribe(result => {
+        this.offer.set(result.offer)
+      })
+  }
+
+  protected readonly getWidthByRating = getWidthByRating;
 }
