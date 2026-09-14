04a6140860eea67a3e66ba466c2cdf4764c9ae6c
Sergey Malyuga <sergey@malyuga.ru>
2026-09-14 16:02:24 +0300

    feat(utils): добавить в метод получения ширины рейтинга входящий параметр типа undefined

--- src/app/core/utils/width-by-rating.ts
+++ src/app/core/utils/width-by-rating.ts
@@ -1,4 +1,4 @@
-export function getWidthByRating(value: number): number {
+export function getWidthByRating(value: number | undefined): number {
   const validValue = value ?? 0;
   return Math.floor(validValue * 20);
 }
