722aa292002644425e9f4dad926acf06f468606c
Sergey Malyuga <sergey@malyuga.ru>
2026-09-14 13:14:32 +0300

    feat(favorite-offer): добавить actions изменения избранности оффера

--- src/app/store/favoritre-offer/actions/favorite-offer.actions.ts
+++ src/app/store/favoritre-offer/actions/favorite-offer.actions.ts
@@ -1,5 +1,5 @@
 import {createAction, props} from '@ngrx/store';
-import {OfferPreview} from '../../../core/models/offers';
+import {Offer, OfferPreview} from '../../../core/models/offers';
 import {HttpErrorResponse} from '@angular/common/http';
 
 export const loadFavoriteOffers = createAction('[Favorite Offer] Load Favorite Offers');
@@ -7,3 +7,12 @@ export const loadFavoriteOffersSuccess = createAction('[Favorite Offer] Load Fav
   props<{ offers: OfferPreview[] }>());
 export const loadFavoriteOffersFailure = createAction('[Favorite Offer] Load Favorite Offers Failure',
   props<{ error: HttpErrorResponse }>());
+
+export const toggleFavoriteOffer = createAction(
+  '[Favorite Offer] Toggle Offer',
+  props<{ offerId: string; isFavorite: boolean }>(),
+);
+export const toggleFavoriteOfferSuccess = createAction('[Favorite Offer Effects] Toggle Offer Success',
+  props<{ offer: Offer }>());
+export const toggleFavoriteOfferFailure = createAction('[Favorite Offer Effects] Toggle Offer Failure',
+  props<{ error: HttpErrorResponse }>())
